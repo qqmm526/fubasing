1521b0a64072dab5ca56a204fa74e3f25d889d7a
qqmm526 <41115047+qqmm526@users.noreply.github.com>
2023-01-30 22:41:43 +0800

    Update autosing.py

--- autosing.py
+++ autosing.py
@@ -17,7 +17,7 @@ def start(cookie, username):
 
         #flb_url = get_addr()
         
-        flb_url = www.wnflb2023.com
+        flb_url = "www.wnflb2023.com"
         headers = {'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
                    'Accept - Encoding': 'gzip, deflate',
                    'Accept-Language': 'zh-CN,zh;q=0.9',
